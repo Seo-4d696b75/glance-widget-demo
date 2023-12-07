56e23840cf6acadead852a6623ac9e86daca2f7a
Seo-4d696b75 <s.kaoru509@gmail.com>
2023-12-07 12:09:25 +0900

    fix: cornerRadius

--- widget/src/main/java/com/seo4d696b75/android/glance_widget_demo/widget/screen/HelloScreen.kt
+++ widget/src/main/java/com/seo4d696b75/android/glance_widget_demo/widget/screen/HelloScreen.kt
@@ -2,10 +2,11 @@ package com.seo4d696b75.android.glance_widget_demo.widget.screen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import androidx.glance.ColorFilter
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
-import androidx.glance.appwidget.cornerRadius
-import androidx.glance.background
+import androidx.glance.Image
+import androidx.glance.ImageProvider
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.fillMaxSize
@@ -19,18 +20,26 @@ fun HelloScreen(
     modifier: GlanceModifier = GlanceModifier,
 ) {
     Box(
-        modifier = modifier
-            .fillMaxSize()
-            .background(GlanceTheme.colors.surface)
-            .cornerRadius(8.dp)
-            .padding(4.dp),
-        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize(),
     ) {
-        Text(
-            text = glanceString(id = R.string.widget_hello_message),
-            style = TextStyle(
-                color = GlanceTheme.colors.onSurface,
-            )
+        Image(
+            provider = ImageProvider(R.drawable.widget_background),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(GlanceTheme.colors.surface),
+            modifier = GlanceModifier.fillMaxSize(),
         )
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(4.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = glanceString(id = R.string.widget_hello_message),
+                style = TextStyle(
+                    color = GlanceTheme.colors.onSurface,
+                )
+            )
+        }
     }
 }
