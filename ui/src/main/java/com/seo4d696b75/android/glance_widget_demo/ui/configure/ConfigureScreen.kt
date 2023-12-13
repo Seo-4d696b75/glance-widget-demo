108b4558e72c65aba39bd8db8ab762f749f16655
Seo-4d696b75 <s.kaoru509@gmail.com>
2023-12-13 17:35:32 +0900

    refactor: configure ui

--- ui/src/main/java/com/seo4d696b75/android/glance_widget_demo/ui/configure/ConfigureScreen.kt
+++ ui/src/main/java/com/seo4d696b75/android/glance_widget_demo/ui/configure/ConfigureScreen.kt
@@ -7,13 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,7 +49,7 @@ fun ConfigureScreen(
         input = uiState.input,
         isError = uiState.isError,
         onInputChanged = viewModel::onInputChanged,
-        addWidget = viewModel::complete,
+        complete = viewModel::complete,
         modifier = modifier,
     )
 }
@@ -58,7 +60,7 @@ fun ConfigureScreen(
     input: String,
     isError: Boolean,
     onInputChanged: (String) -> Unit,
-    addWidget: () -> Unit,
+    complete: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     AppTheme {
@@ -86,26 +88,34 @@ fun ConfigureScreen(
                         style = MaterialTheme.typography.titleMedium,
                     )
                     Spacer(
-                        modifier = Modifier.height(24.dp),
+                        modifier = Modifier.height(12.dp),
                     )
-                    TextField(
+                    OutlinedTextField(
                         value = input,
                         onValueChange = onInputChanged,
+                        isError = isError,
+                        label = {
+                            Text(text = stringResource(id = R.string.configure_text_field_label))
+                        },
+                        maxLines = 1,
                         keyboardOptions = KeyboardOptions(
                             keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done,
                         ),
-                        isError = isError,
-                        modifier = Modifier.width(180.dp),
+                        keyboardActions = KeyboardActions(
+                            onDone = { complete() },
+                        ),
+                        modifier = Modifier.width(120.dp),
                     )
                     Spacer(
-                        modifier = Modifier.height(48.dp),
+                        modifier = Modifier.height(36.dp),
                     )
                     Button(
-                        onClick = addWidget,
+                        onClick = complete,
                         enabled = !isError,
                     ) {
                         Text(
-                            text = stringResource(id = R.string.configure_complete_label),
+                            text = stringResource(id = R.string.configure_complete_button_label),
                         )
                     }
                 }
@@ -121,7 +131,7 @@ private fun ConfigureScreenPreview() {
         input = "10",
         isError = false,
         onInputChanged = {},
-        addWidget = {},
+        complete = {},
     )
 }
 
@@ -132,6 +142,6 @@ private fun ConfigureScreenPreview_invalid() {
         input = "zero",
         isError = true,
         onInputChanged = {},
-        addWidget = {},
+        complete = {},
     )
 }
